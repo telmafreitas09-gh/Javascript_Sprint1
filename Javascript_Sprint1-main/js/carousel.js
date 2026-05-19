f28d760270cb3046b92399bbfd3b663aaf853ee8
telmafreitas09-gh <telmafreitas09@gmail.com>
2026-05-19 15:06:24 -0300

    Finalizando carousel

--- Javascript_Sprint1-main/js/carousel.js
+++ Javascript_Sprint1-main/js/carousel.js
@@ -9,7 +9,11 @@ let carouselArr = [];
 //class Carousel
 class Carousel {
 
-    
+    constructor(image,title,url){
+        this.image=image;
+        this.title=title;
+        this.url=url;
+    }
       
     static Start(arr){
         if(arr){
@@ -17,8 +21,13 @@ class Carousel {
             if(arr.length > 0){
                 Carousel._sequence = 0;
                 Carousel._size = arr.length;
-                Carousel.Next(); //start
-                Carousel._interval = setInterval(function(){ Carousel.Next(); },5000);
+                Carousel.Show(); //start
+                Carousel._interval = setInterval(function(){ Carousel.Next(); },5000);  
+
+
+                document.getElementById("avance_btn").addEventListener("click",function(){Carousel.Next();}); 
+
+                 document.getElementById("volte_btn").addEventListener("click",function(){Carousel.Next();}); 
             }
             
         } else {
@@ -26,7 +35,46 @@ class Carousel {
         }
     }
 
-    static Next(){
+    static Show(){
+        let item = carouselArr[Carousel._sequence]; 
+        
+        let carousel_div = document.getElementById("carousel");
+
+        let carousel_title = document.getElementById("carousel-title");
+
+        carousel_div.style.backgroundImage = `url('img/${item.image}')`;
+
+        carousel_div.style.backgroundSize = "cover";
+
+        carousel_div.style.backgroundPosition = "center";
+
+        carousel_div.style.transition = "all .4s ease-in-out";
+
+        carousel_title.innerHTML = `<a href = "${item.url}">${item.title}</a>`;
+        
         
     }
+
+        static Next(){
+            Carousel._sequence++;
+
+            if( Carousel._sequence >= Carousel._size){
+            Carousel._sequence = 0;
+        }
+
+         Carousel.Show();
+
+        }
+
+        static Prev(){
+            Carousel._sequence--;
+
+            if(Carousel._sequence<0){
+            Carousel._sequence = Carousel._size -1;  
+            }
+
+         Carousel.Show(); 
+
+        }
+
 };
